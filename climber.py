b353d113a68f7a764033ea767ae644e890daf38d
shaunmac611 <shaunmac611@gmail.com>
2021-11-28 14:30:13 -0500

    Created a Climber object to pull user data
    Climber object pulls best grade send and top 10 grade average send

---
+++ climber.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 27 20:41:05 2021
+
+@author: Shaun
+"""
+import pandas as pd
+
+"""Dictionaries used to convert climbing grades to an integer representation
+of difficulty"""
+
+YDS_DICT={"N/A":"N/A",'3-4':0,'5':1,'5.0':1,'5.1':2,'5.2':3,'5.3':4,'5.4':5,
+          '5.5':6,'5.6':7,'5.7':8,'5.8':9,'5.9':10,
+          '5.10a':11,'5.10b':12,'5.10c':13,'5.10d':14,
+          '5.11a':15,'5.11b':16,'5.11c':17,'5.11d':18,
+          '5.12a':19,'5.12b':20,'5.12c':21,'5.12d':22,
+          '5.13a':23,'5.13b':24,'5.13c':25,'5.13d':26,
+          '5.14a':27,'5.14b':28,'5.14c':29,'5.14d':30,
+          '5.15a':31,'5.15b':32,'5.15c':33,'5.15d':34}
+
+FRENCH_2_YDS={"N/A":"N/A","1":"5.0","2":"5.1","3":"5.2","3+":"5.3","4":"5.4","4a":"5.4","4b":"5.5",
+              "4c":"5.6","5a":"5.7","5b":"5.8","5c":"5.9",
+              "6a":"5.10a","6a+":"5.10b","6b":"5.10c","6b+":"5.10d",
+              "6c":"5.11a","6c+":"5.11c","7a":"5.11d",
+              "7a+":"5.12a","7b":"5.12b","7b+":"5.12c","7c":"5.12d",
+              "7c+":"5.13a","8a":"5.13b","8a+":"5.13c","8b":"5.13d",
+              "8b+":"5.14a","8c":"5.14b","8c+":"5.14c","9a":"5.14d",
+              "9a+":"5.15a","9b":"5.15b","9b+":"5.15c","9c":"5.15d"}
+
+BOULDER_DICT={"N/A":"N/A",'VB':12,'V0':13,'V1':14,'V2':15,'V3':16,'V4':17,'V5':18,
+              'V6':19,'V7':20,'V8':21,'V9':22,'V10':23,'V11':24,'V12':25,
+              'V13':26,'V14':27,'V15':28,'V16':29,'V17':30}
+
+FRENCH_2_HUECO={"N/A":"N/A","3":"V0-","4-":"V0","4":"V0","4+":"V1","5-":"V1",
+                "5":"V1","5+":"V2","6A":"V3","6A+":"V3","6B":"V4","6B+":"V4",
+                "6C":"V5","6C+":"V5","7A":"V6","7A+":"V7","7B":"V8",
+                "7B+":"V8","7C":"V9","7C+":"V10","8A":"V11","8A+":"V12",
+                "8B":"V13","8B+":"V14","8C":"V15","8C+":"V16","9A":"V17"}
+
+"""Color options at the cliffs"""
+COLORS=['Lime Green','Purple','Yellow','Pink','Grey','Blue','White',
+        'Tan','Red','Black']
+
+"""Default option for each climber info, not all are required"""
+CLASS_NAMES_DEFAULT = {'zlags':0, 'meters climbed':0, 'total points':0,
+                    'best lead grade':'N/A', 'best boulder grade':'N/A'}
+
+class Climber:
+
+    def __init__(self, climber_soup):
+        self.soup = climber_soup
+        self.name = self.soup.find("title").get_text()[:-1*len(' vertical-life profile')]
+        self.app_id = 'temp'#self.soup.find("meta", {"name":"al:ios:url"})['content']
+        class_values = [div_class.get_text() for div_class in self.soup.find_all("div", {"class": "value"})][2:]
+        class_names = [div_class.get_text() for div_class in self.soup.find_all("div", {"class": "name"})]
+        climber_info = {class_names[i]: class_values[i] for i in range(len(class_names))}
+        all_class_names = CLASS_NAMES_DEFAULT
+        for i in list(climber_info.keys()):
+            all_class_names[i] = climber_info[i]
+        
+        self.zlags = int(all_class_names['zlags'].replace(" ",""))
+        self.meters_climbed = int(all_class_names['meters climbed'].replace(" ",""))
+        self.total_points = int(all_class_names['total points'].replace(" ",""))
+        
+        route_data, top_routes, top_boulders = self.get_top_sends()
+        self.route_data = route_data
+        self.top_routes = top_routes
+        self.top_boulders = top_boulders
+        
+        if not top_routes.empty:
+            self.best_lead_grade = top_routes['grade'].iloc[0]
+            if len(top_routes['grade'])>=3:
+                self.third_best_lead_grade = top_routes['grade'].iloc[2]
+            else:
+                self.third_best_lead_grade = top_routes['grade'].iloc[-1]
+            self.top_ten_avg_lead_grade = list(YDS_DICT.keys())[list(YDS_DICT.values()).index(round(top_routes['difficulty'].mean()))]
+        else:
+            self.best_lead_grade = "N/A"
+            self.third_best_lead_grade = "N/A"
+            self.top_ten_avg_lead_grade = "N/A"
+        if not top_boulders.empty:
+            self.best_boulder_grade = top_boulders['grade'].iloc[0]
+            if len(top_boulders['grade'])>=3:
+                self.third_best_boulder_grade = top_boulders['grade'].iloc[2]
+            else:
+                self.third_best_boulder_grade = top_boulders['grade'].iloc[-1]
+            self.top_ten_avg_boulder_grade = list(BOULDER_DICT.keys())[list(BOULDER_DICT.values()).index(round(top_boulders['difficulty'].mean()))]
+        else:
+            self.best_boulder_grade = "N/A"
+            self.third_best_boulder_grade = "N/A"
+            self.top_ten_avg_boulder_grade = "N/A"
+        
+    def to_df(self):
+        d = {'app_id':[self.app_id],
+             'name':[self.name],
+             'zlags':[self.zlags],
+             'meters_climbed':[self.meters_climbed],
+             'total_points':[self.total_points],
+             
+             'best_lead_grade':[self.best_lead_grade],
+             'best_boulder_grade':[self.best_boulder_grade],
+             'best_lead_grade_num':YDS_DICT[self.best_lead_grade],
+             'best_boulder_grade_num':BOULDER_DICT[self.best_boulder_grade],
+             
+             'third_best_lead_grade':[self.third_best_lead_grade],
+             'third_best_boulder_grade':[self.third_best_boulder_grade],
+             'third_best_lead_grade_num':YDS_DICT[self.third_best_lead_grade],
+             'third_best_boulder_grade_num':BOULDER_DICT[self.third_best_boulder_grade],
+             
+             'top_ten_avg_lead_grade':[self.top_ten_avg_lead_grade],
+             'top_ten_avg_boulder_grade':[self.top_ten_avg_boulder_grade],
+             'top_ten_avg_lead_grade_num':YDS_DICT[self.top_ten_avg_lead_grade],
+             'top_ten_avg_boulder_grade_num':BOULDER_DICT[self.top_ten_avg_boulder_grade]
+             }
+        return pd.DataFrame(d)
+    
+    def get_top_sends(self, count=10):
+        climbs = self.soup.find_all(class_="route")
+        columns = ['color', 'grade', 'difficulty', 'route_type', 'location']
+        route_data = pd.DataFrame(columns = columns)
+        lic_climbs = [climb for climb in climbs if 'The Cliffs at LIC' in climb.get_text()]
+        for climb in lic_climbs:
+            items = str(climb).split('\n')
+            grade=items[2].split(" ")[-1]
+            color=items[2].split(" ")[-2]
+            location=items[5].split(">")[1].split('<')[0]
+            if '5.' in climb.get_text() or '3-4' in climb.get_text():
+                route_type = 'roped'
+                difficulty = YDS_DICT[grade]
+            else:
+                route_type = 'boulder'
+                difficulty = BOULDER_DICT[grade]
+            route_data = append_dataframes(route_data, pd.DataFrame([[color,grade,difficulty,route_type,location]], columns=columns))
+        top_routes = route_data.loc[route_data['route_type']=='roped'].sort_values(by='difficulty', axis=0, ascending=False)[:count]
+        top_boulders = route_data.loc[route_data['route_type']=='boulder'].sort_values(by='difficulty', axis=0, ascending=False)[:count]
+        
+        return route_data, top_routes.reset_index(), top_boulders.reset_index()
+
+    def to_string(self):
+        return self.name + ': Best Lead:' + self.best_lead_grade + '  |  Best Boulder ' + self.best_boulder_grade
+
+def append_dataframes(big_df, small_df):
+    if big_df.empty:
+        big_df = small_df.copy()
+    else:
+        big_df = big_df.append(small_df)
+    return big_df
--- vertical_life_scrape.py
+++ vertical_life_scrape.py
@@ -7,17 +7,60 @@ Created on Sat Mar 27 19:00:44 2021
 import pandas as pd
 import os
 import urllib.request as url_request
-from urllib.error import HTTPError
+from urllib.error import HTTPError, URLError
 from bs4 import BeautifulSoup
 
 from climb import Climb
+from climber import Climber
 
 VL_SITE = "https://www.vertical-life.info/"
 GYM_SITE = "en/indoor/the-cliffs-at-lic/"
-DATA_FILE = 'cliffs_climbs.xlsx'
+
+def cliffs_climber_scrape():
+    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+    headers={'User-Agent':user_agent,} 
+
+    request = url_request.Request(VL_SITE+GYM_SITE,None,headers)
+    webUrl = url_request.urlopen(request)
+    soup = BeautifulSoup(webUrl, 'html.parser')
+    
+    climber_data = loop_through_climbers(soup, headers, constant_export = True, suppress_print=False)
+    return climber_data
+
+def loop_through_climbers(soup, headers, constant_export = True, suppress_print=True):
+    climber_data = pd.DataFrame()
+    count=0
+    for link in soup.find_all('a'):
+        if ('/en/climbers/' in link.get('href')) and (not '\n\n\n\n\n' in link.get_text()):
+            count=count+1
+            if count>20:
+                climber_url = VL_SITE[:-1] + link.get('href')
+                if climber_url != False:
+                    try:
+                        climber_request = url_request.Request(climber_url,None,headers)
+                        climber_webUrl = url_request.urlopen(climber_request)
+                        climber_soup = BeautifulSoup(climber_webUrl, 'html.parser')
+                        try:
+                            climber_o = Climber(climber_soup)
+                            climber_data = append_dataframes(climber_data, climber_o.to_df())
+                            if constant_export:
+                                climber_data.to_excel('cliffs_climbers.xlsx', index=False)
+                            if count%100==0 and not suppress_print:
+                                print('Climber Count: ' + str(count))
+                        except TypeError:
+                            print('None Type: ' + str(count))
+                            continue
+                    except URLError:
+                        print('URL Error: ' + str(count))
+                        continue
+    return climber_data
 
 def cliffs_climb_scrape():
-    webUrl = url_request.urlopen(VL_SITE+GYM_SITE)
+    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+    headers={'User-Agent':user_agent,} 
+
+    request = url_request.Request(VL_SITE+GYM_SITE,None,headers)
+    webUrl = url_request.urlopen(request)
     soup = BeautifulSoup(webUrl, 'html.parser')
     
     count=0
@@ -34,22 +77,22 @@ def cliffs_climb_scrape():
     export_climb_data(climb_data_df)
 
 def export_climb_data(climb_data_df):
-    if os.path.isfile(DATA_FILE):
-        climb_data_file = pd.read_excel(DATA_FILE, usecols=climb_data_df.columns, dtype={'app_id':str})
+    if os.path.isfile('cliffs_climbs.xlsx'):
+        climb_data_file = pd.read_excel('cliffs_climbs.xlsx', usecols=climb_data_df.columns, dtype={'app_id':str})
         climb_data_file['current_state']=False
         climb_data_file = append_dataframes(climb_data_file, climb_data_df)
         unique_cols = ['app_id', 'type', 'grade', 'color', 'image', 'first_ascent','num_grade']
         climb_data_file = climb_data_file.drop_duplicates(subset=unique_cols, keep='last')
-        climb_data_file.to_excel(DATA_FILE, index=False)
+        climb_data_file.to_excel('cliffs_climbs.xlsx', index=False)
     else:
-        climb_data_df.to_excel(DATA_FILE)
+        climb_data_df.to_excel('cliffs_climbs.xlsx')
 
 def loop_through_climbs(routes, climb_type):
     route_data = pd.DataFrame()
     for route in routes:
-        climb_url = is_climb_url(route)
-        if climb_url != False:
-            climb_soup = BeautifulSoup(climb_url, 'html.parser')
+        climb_soup = is_climb_url(route)
+        if climb_soup != False:
+            #climb_soup = BeautifulSoup(climb_url, 'html.parser')
             climb = Climb(climb_soup, climb_type)
             route_data = append_dataframes(route_data, climb.to_df())
     return route_data
@@ -58,8 +101,12 @@ def is_climb_url(route):
     if 'href=' in str(route):
         climb_url = str(route).split('href=')[1].split('>')[0][1:-1]
         try:
-            climb_url = url_request.urlopen(VL_SITE+climb_url)
-            return climb_url
+            user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+            headers={'User-Agent':user_agent,} 
+            request = url_request.Request(VL_SITE+climb_url,None,headers)
+            webUrl = url_request.urlopen(request)
+            climb_soup = BeautifulSoup(webUrl, 'html.parser')
+            return climb_soup
         except HTTPError:
             return False
     else:
