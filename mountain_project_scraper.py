669947e114173b951701b9c44efe5280351fdeaa
Shaun McCarthy <ShaunMac611@gmail.com>
2021-12-22 10:25:05 -0500

    Added dictionary of locations and defaulted constant export to false

--- mountain_project_scraper.py
+++ mountain_project_scraper.py
@@ -4,8 +4,6 @@ Created on Fri Dec  3 17:41:26 2021
 
 @author: Shaun
 
-Potential Improvements:
-    Add Climber link to object
 """
 import pandas as pd
 
@@ -13,22 +11,33 @@ import helper
 from climb import MP_Climb
 from climber import MP_Climber
 
-location_dict = {'Red River Gorge':'105841134', 'New River Gorge':'105855991',
+LOCATION_DICT = {'Red River Gorge':'105841134', 'New River Gorge':'105855991',
                  'Yosemite':'105833388', 'Gunks':'105798167', 'Boulder Canyon':'105744222',
                  'Joshua Tree':'105720495', 'Eldorado Canyon':'105744246', 'Rumney':'105867829',
-                 'Red Rocks':'105731932'}
-def test_main():
-    for location in ['Red Rocks', 'Rumney', 'Eldorado Canyon', 'Yosemite']:
-        location_code = location_dict[location]
-        print('Pulling Data for: ' + location)
-        climb_data, climber_data = loop_through_areas(location_code, location)
+                 'Red Rocks':'105731932', 'Horseshoe Canyon':'105903004', 'Indian Creek':'105716763',
+                 'Smith Rock':'105788989', 'Squamish':'105798170'}
+SPORT_CRAGS = ['Rumney', 'New River Gorge', 'Red Rocks', 'Boulder Canyon',
+               'Red River Gorge', 'Horseshoe Canyon', 'Smith Rock']
+TRAD_CRAGS = ['Squamish', 'Joshua Tree', 'Eldorado Canyon', 'Yosemite', 'Gunks',
+              'Indian Creek']
 
-def loop_through_areas(location_code, location, climb_data=pd.DataFrame(), climber_data=pd.DataFrame(), pull_climber_data=True):
-    soup = helper.is_climb_url(get_mp_link(location_code))
+def test_main():
+    for location in SPORT_CRAGS:
+        location_code = LOCATION_DICT[location]
+        print('Pulling Data for: ' + location)
+        climb_data, climber_data = loop_through_areas(location_code, location, climb_filter=['Sport'])
+    for location in TRAD_CRAGS:
+        location_code = LOCATION_DICT[location]
+        print('Pulling Data for: ' + location)
+        climb_data, climber_data = loop_through_areas(location_code, location, climb_filter=['Trad'])
+
+def loop_through_areas(location_code, location, climb_data=pd.DataFrame(), climber_data=pd.DataFrame(), pull_climber_data=True, climb_filter=['sport','trad','tr']):
+    climb_filter = [i.title() for i in climb_filter]
+    soup = helper.is_climb_url(get_mp_link(location_code, climb_filter))
     if soup:
         climb_total = int(soup.find_all(class_="float-md-left")[0].get_text().split('Results 1 to ')[1].split(' of')[0])
         if climb_total<1000:
-            climb_data, climber_data = loop_through_climbs(soup, location, location_code, pull_climber_data, climb_total)
+            climb_data, climber_data = loop_through_climbs(soup, location, location_code, pull_climber_data, climb_total, climb_filter=climb_filter)
         else:
             soup_parent = helper.is_climb_url("https://www.mountainproject.com/area/" + location_code)
             if soup_parent:
@@ -42,9 +51,11 @@ def loop_through_areas(location_code, location, climb_data=pd.DataFrame(), climb
                     climber_data = helper.append_dataframes(climber_data, area_climber_data)
                     climb_data.to_excel(location+'_climbs.xlsx')
                     climber_data.to_excel(location+'_climbers.xlsx')
+        climb_data.to_excel(location+'_climbs.xlsx')
+        climber_data.to_excel(location+'_climbers.xlsx')
     return climb_data, climber_data
 
-def loop_through_climbs(soup, location, location_code, pull_climber_data, climb_total, constant_export = False, suppress_print=False):
+def loop_through_climbs(soup, location, location_code, pull_climber_data, climb_total, constant_export = False, suppress_print=False, climb_filter=['sport','trad','tr']):
     climb_data = pd.DataFrame()
     climber_data = pd.DataFrame()
     link_list = []
@@ -59,7 +70,7 @@ def loop_through_climbs(soup, location, location_code, pull_climber_data, climb_
                     climber_data, link_list = get_ascent_data(route, climber_data,
                                                               link_list, location,
                                                               constant_export,
-                                                              True)
+                                                              True, climb_filter=climb_filter)
                 climb_data = helper.append_dataframes(climb_data, route.to_df())
                 if constant_export:
                     climb_data.to_excel(location+'_climbs_'+location_code+'.xlsx', index=False)
@@ -67,11 +78,11 @@ def loop_through_climbs(soup, location, location_code, pull_climber_data, climb_
                     print('=====Climb Count: ' + str(count))
     return climb_data, climber_data
 
-def get_ascent_data(route, climber_data, link_list, location, constant_export, suppress_print):
+def get_ascent_data(route, climber_data, link_list, location, constant_export, suppress_print, climb_filter=['sport','trad','tr']):
     climber_count=0
-    soup = helper.is_climb_url(route.ascents_link)
-    if soup:
-        tick_options = soup.find_all(class_="table table-striped")
+    route_soup = helper.is_climb_url(route.ascents_link)
+    if route_soup:
+        tick_options = route_soup.find_all(class_="table table-striped")
         if len(tick_options)>=4:
             ticks = tick_options[3]
             climber_links = ticks.find_all('a')
@@ -79,7 +90,7 @@ def get_ascent_data(route, climber_data, link_list, location, constant_export, s
                 if not (link in link_list):
                     link_list = link_list + [link]
                     climber_soup = helper.is_climb_url(link.get('href')+'/tick-export')
-                    climber_o = MP_Climber(climber_soup, name=link.get_text(), location_filter=location)
+                    climber_o = MP_Climber(climber_soup, name=link.get_text(), location_filter=location, sport_definition=climb_filter)
                     climber_data = helper.append_dataframes(climber_data, climber_o.to_df())
                     climber_count=climber_count+1
                     if constant_export:
